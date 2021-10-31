8e6fb46c729569cc2388023cc0eb38c909bcae90
p0wnz <p0wnz@github.com>
2021-10-30 23:00:27 -0400

    added automatic back feature

--- upload.php
+++ upload.php
@@ -37,6 +37,7 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	header('Location: index.php');
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
